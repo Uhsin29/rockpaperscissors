f57dcbd69a70d10ad3d49ce5632ee16b77fcc0b0
Glitch (justin-rockpaperscissors) <none>
2022-01-13 00:26:43 +0000

    👚⛰️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+$(".play").click(function() {
+  let choice = $(".input").val();
+  $(".userChoice").text(`${choice}`);
+  let computerNumber;
+  computerNumber = Math.random() * 3;
+  computerNumber = Math.ceil(computerNumber);
+  $(".computerChoice").text(computerNumber);
+});
+$(".rock").click(function() {
+  $(".userChoice").text("You chose ROCK");
+  let computerNumber = Math.random() * 3;
+  computerNumber = Math.ceil(computerNumber);
+  $(".computerChoice").text(computerNumber);
+  if (computerNumber === "2") {$(".computerChoice").text("Paper")}
+  else if (computerNumber === "3") {$(".computerChoice").text("Rock")}
+  else {$(".computerChoice").text("Rock")}
+  });
 
+$(".paper").click(function() {
+  $(".userChoice").text("You chose PAPER");
+  let computerNumber = Math.random() * 3;
+  computerNumber = Math.ceil(computerNumber);
+  $(".computerChoice").text(computerNumber);
+});
+
+$(".scissors").click(function() {
+  $(".userChoice").text("You chose SCISSORS");
+  let computerNumber = Math.random() * 3;
+  computerNumber = Math.ceil(computerNumber);
+  $(".computerChoice").text(computerNumber);
+});
